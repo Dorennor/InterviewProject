050f4d39b3b5d2e6a76a62e546aa22033bb21200
Vladimir <vova.rud.00@gmail.com>
2021-08-15 02:14:03 +0300

    0.1
    Creating models.

---
+++ InterviewProject/Models/AccountsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterviewProject.Models
+{
+    public sealed class AccountsDbContext : IdentityDbContext
+    {
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public AccountsDbContext(DbContextOptions<AccountsDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
